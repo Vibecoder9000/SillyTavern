41ae60aad58624c2c9cf171a6641953b36df65e8
Luke Scala <linkpharm8888@gmail.com>
2025-08-30 00:52:15 -0500

    remove duplicate import

--- src/endpoints/avatars.js
+++ src/endpoints/avatars.js
@@ -9,7 +9,6 @@ import { sync as writeFileAtomicSync } from 'write-file-atomic';
 
 import { getImages, tryParse } from '../util.js';
 import { getFileNameValidationFunction } from '../middleware/validateFileName.js';
-import cacheBuster from '../middleware/cacheBuster.js';
 import { applyAvatarCropResize } from './characters.js';
 import { invalidateThumbnail } from './thumbnails.js';
 
